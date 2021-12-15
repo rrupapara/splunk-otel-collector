5c9bc88062d766eebe1f9e66087ba243a1ac65a0
Ravi Rupapara <rrupapara@splunk.com>
2021-12-15 12:45:25 +0530

    Adopt jmx receiver

--- internal/components/components.go
+++ internal/components/components.go
@@ -44,6 +44,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/fluentforwardreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/jaegerreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/jmxreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/journaldreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/kafkametricsreceiver"
@@ -101,6 +102,7 @@ func Get() (component.Factories, error) {
 		filelogreceiver.NewFactory(),
 		jaegerreceiver.NewFactory(),
 		journaldreceiver.NewFactory(),
+		jmxreceiver.NewFactory(),
 		k8sclusterreceiver.NewFactory(),
 		kafkametricsreceiver.NewFactory(),
 		kafkareceiver.NewFactory(),
